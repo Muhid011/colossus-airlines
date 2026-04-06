c0a3196be6937fdde1773ae5e39f412b08632393
Hamza Muhidin <muhid011@umn.edu>
2026-04-05 19:15:29 -0500

    Add files via upload

---
+++ solution.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SEATS 48
+#define NAME_LEN 20
+
+struct seat {
+    int id;
+    int assigned;
+    char first[NAME_LEN];
+    char last[NAME_LEN];
+};
+
+// function list
+void showMainMenu();
+void showFlightMenu();
+void initSeats(struct seat s[]);
+void showEmptyCount(struct seat s[]);
+void showEmptyList(struct seat s[]);
+void showAlphabetical(struct seat s[]);
+void assignSeat(struct seat s[]);
+void deleteSeat(struct seat s[]);
+
+int main() {
+    struct seat outbound[SEATS];
+    struct seat inbound[SEATS];
+
+    char choice1, choice2;
+
+    initSeats(outbound);
+    initSeats(inbound);
+
+    while (1) {
+        showMainMenu();
+        printf("Choose: ");
+        scanf(" %c", &choice1);
+
+        if (choice1 == 'c') {
+            printf("Goodbye!\n");
+            break;
+        }
+
+        struct seat *flight;
+
+        if (choice1 == 'a')
+            flight = outbound;
+        else if (choice1 == 'b')
+            flight = inbound;
+        else {
+            printf("Invalid option\n");
+            continue;
+        }
+
+        while (1) {
+            showFlightMenu();
+            printf("Choose: ");
+            scanf(" %c", &choice2);
+
+            if (choice2 == 'f')
+                break;
+
+            switch (choice2) {
+                case 'a': showEmptyCount(flight); break;
+                case 'b': showEmptyList(flight); break;
+                case 'c': showAlphabetical(flight); break;
+                case 'd': assignSeat(flight); break;
+                case 'e': deleteSeat(flight); break;
+                default: printf("Invalid option\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
+// ---------------- FUNCTIONS ----------------
+
+void showMainMenu() {
+    printf("\n====== Colossus Airlines ======\n");
+    printf("a) Outbound Flight\n");
+    printf("b) Inbound Flight\n");
+    printf("c) Quit\n");
+}
+
+void showFlightMenu() {
+    printf("\n------ Flight Menu ------\n");
+    printf("a) Show number of empty seats\n");
+    printf("b) Show list of empty seats\n");
+    printf("c) Show alphabetical list of seats\n");
+    printf("d) Assign a seat\n");
+    printf("e) Delete a seat\n");
+    printf("f) Return to main menu\n");
+}
+
+void initSeats(struct seat s[]) {
+    for (int i = 0; i < SEATS; i++) {
+        s[i].id = i + 1;
+        s[i].assigned = 0;
+    }
+}
+
+void showEmptyCount(struct seat s[]) {
+    int count = 0;
+    for (int i = 0; i < SEATS; i++)
+        if (s[i].assigned == 0)
+            count++;
+
+    printf("Empty seats: %d\n", count);
+}
+
+void showEmptyList(struct seat s[]) {
+    printf("Empty seats: ");
+    for (int i = 0; i < SEATS; i++)
+        if (s[i].assigned == 0)
+            printf("%d ", s[i].id);
+
+    printf("\n");
+}
+
+// alphabetical list
+void showAlphabetical(struct seat s[]) {
+    struct seat temp[SEATS];
+    int count = 0;
+
+    // copy assigned seats
+    for (int i = 0; i < SEATS; i++) {
+        if (s[i].assigned == 1) {
+            temp[count] = s[i];
+            count++;
+        }
+    }
+
+    // bubble sort by last name
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (strcmp(temp[j].last, temp[j+1].last) > 0) {
+                struct seat t = temp[j];
+                temp[j] = temp[j+1];
+                temp[j+1] = t;
+            }
+        }
+    }
+
+    printf("Seats (alphabetical):\n");
+    for (int i = 0; i < count; i++) {
+        printf("Seat %d: %s %s\n",
+            temp[i].id,
+            temp[i].first,
+            temp[i].last);
+    }
+}
+
+void assignSeat(struct seat s[]) {
+    int num;
+
+    printf("Enter seat number (1-48) or 0 to cancel: ");
+    scanf("%d", &num);
+
+    if (num == 0) return;
+
+    if (num < 1 || num > SEATS || s[num-1].assigned == 1) {
+        printf("Invalid or seat taken.\n");
+        return;
+    }
+
+    printf("First name: ");
+    scanf("%s", s[num-1].first);
+
+    printf("Last name: ");
+    scanf("%s", s[num-1].last);
+
+    s[num-1].assigned = 1;
+
+    printf("Seat assigned!\n");
+}
+
+void deleteSeat(struct seat s[]) {
+    int num;
+
+    printf("Enter seat number from (1-48) or 0 to cancel: ");
+    scanf("%d", &num);
+
+    if (num == 0) return;
+
+    if (num < 1 || num > SEATS || s[num-1].assigned == 0) {
+        printf("Seat already empty.\n");
+        return;
+    }
+
+    s[num-1].assigned = 0;
+    printf("Seat cleared.\n");
+}
